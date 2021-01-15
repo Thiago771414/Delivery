ea5d2888fe7efb4335e6282f6956c21091f82844
Thiago <thiso2@hotmail.com>
2021-01-15 15:54:46 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositores/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositores/OrderRepository.java
@@ -1,10 +1,17 @@
 package com.devsuperior.dsdeliver.repositores;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsdeliver.entities.Order;
 
 //JpaRepository faz o crud na entidade
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+	
+	//escrever consulta
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrderSwitchProducts();
 }
