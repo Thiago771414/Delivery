a20b75f9bf4e1010a8210bd7f90076844576cf80
Thiago <thiso2@hotmail.com>
2021-04-18 20:26:03 -0300

    Finish front-web-2

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = `http://localhost:8080/products`;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
-    return axios(`${API_URL}/products`)
+    return axios(`http://localhost:8080/products`)
+    //return axios(`${API_URL}/products`)
 }
 
 export function fetchLocalMapBox(local: string) {
@@ -13,5 +14,5 @@ export function fetchLocalMapBox(local: string) {
 }
 
 export function saveOrder (payload: OrderPayload) {
-    return axios.post(`${API_URL}/orders`, payload);
+    return axios.post(`http://localhost:8080/orders`, payload);
 }
